7ffdb68a5fa23011b119294a590e27a1360cebd6
Antoine Vignes <168306302+antoinevignes@users.noreply.github.com>
2025-02-05 16:20:00 +0100

    Ajout dans le login

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -73,6 +73,9 @@ router.post("/login", async (req, res) => {
         id: user._id,
         username: user.username,
         email: user.email,
+        bio: user.bio,
+        firstName: user.firstName,
+        lastName: user.lastName,
       },
     });
   } catch (err) {
