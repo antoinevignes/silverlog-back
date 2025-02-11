aba0a9f5207286a32fae5adc350f71a2dd2447a0
Antoine Vignes <168306302+antoinevignes@users.noreply.github.com>
2025-02-11 19:45:02 +0100

    Modif pour dev

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: process.env.FRONTEND_URL || "https://silverlog.localhost",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
